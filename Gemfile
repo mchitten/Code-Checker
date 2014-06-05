119c748c8f351427085a866c3fb3ee84e2823ffd
Michael Chittenden <mchitten@gmail.com>
2014-06-05 18:57:36 -0400

    Changes for Ruby

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'github_api'
+gem 'sshkey'
+gem 'rubocop'
--- test/pull.sh
+++ test/pull.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-source ~/.bash_profile
+# source ~/.bash_profile
 
 COMPARE_BRANCH="$2"
 COMPARE_REPO="$1"
 
-cd dosomething
-$(git fetch --quiet original && git remote add compare git@sniff:$COMPARE_REPO)
+cat > ~/.ssh/config <<DELIM
+Host mchitten-test
+  HostName github.com
+  User git
+  IdentityFile ~/Sites/Code-Checker/id_rsa_mchitten_test.pub
+DELIM
+
+mkdir -p mchitten
+cd mchitten && git init --quiet
+
+git remote add original_mchitten_test git@mchitten-test:$COMPARE_REPO
+
+$(git fetch --quiet original_mchitten_test && git remote add compare git@mchitten-test:$COMPARE_REPO)
 
 files=""
-for i in $(git fetch --quiet compare && git diff --diff-filter=ACMR original/dev compare/$COMPARE_BRANCH --name-only)
+for i in $(git fetch --quiet compare && git diff --diff-filter=ACMR original_mchitten_test/master compare/$COMPARE_BRANCH --name-only)
 do
+  echo $i
   git checkout compare/$COMPARE_BRANCH -- $i
   files="$files $i"
 done
 
-codercs --report=json $files
-rm -rf *
+rubocop --format=json $files
 git remote remove compare
+cd ../
+# rm -rf mchitten
 
